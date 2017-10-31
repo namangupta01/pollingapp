9cf8bea25e189a174edb4205bf1d7bb8f1233067
Naman Gupta <01namangupta@gmail.com>
2017-10-31 23:43:16 +0530

    part one completed

---
+++ polls/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("Hello World")
+
+
+# Create your views here.
